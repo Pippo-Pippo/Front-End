8bb369e4d273d71a704206ecda225e1b3334649d
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-08 23:14:16 +0900

    Design : nav바 수정

--- jiyoung/js/myChecklist.js
+++ jiyoung/js/myChecklist.js
@@ -1,3 +1,38 @@
+//전역변수
+let isEditMode = false;
+let currentChecklist = "A";
+
+//통신
+$.ajax({
+  url: "https://ppiyong.shop/api/checklist", // 요청을 보낼 URL을 입력합니다.
+  type: "GET", // HTTP 메서드를 지정합니다. 여기서는 GET을 사용합니다.
+  dataType: "json", // 응답 데이터의 유형을 지정합니다. 여기서는 JSON을 사용합니다.
+  success: function (data) {
+    // 요청이 성공적으로 완료되면 실행되는 콜백 함수입니다.
+    // 응답 데이터는 'data' 매개변수에 저장됩니다.
+    console.log("get : ", data);
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    // 요청이 실패하면 실행되는 콜백 함수입니다.
+    console.error(textStatus, errorThrown);
+  },
+});
+
+//목데이터 적용
+$.getJSON("./json/checklist.json", function (data) {
+  $.each(data, function (index, checklist) {
+    console.log(data);
+    const navItem = renderNavItem(checklist.title);
+    $(".my-checklists-container").append(navItem);
+
+    if (checklist.title === "A") {
+      $.each(checklist.task, function (taskIndex, task) {
+        addNewTask(task.content);
+      });
+    }
+  });
+});
+
 $(document).ready(function () {
   // 타이틀 클릭시 수정하게 -> 수정모드에만
   content = document.querySelector("[contenteditable]");
@@ -19,21 +54,24 @@ $(document).ready(function () {
       $("#checklist-input").val(""); // 입력창 초기화
     }
   });
-});
 
-$.getJSON("./json/checklist.json", function (data) {
-  $.each(data, function (index, checklist) {
-    if (checklist.title === "A") {
-      $.each(checklist.task, function (taskIndex, task) {
-        addNewTask(task.content);
-      });
-    }
+  //체크리스트 초기 타이틀
+  initNavItem();
+
+  // 체크리스트 타이틀간 이동 효과
+  $(".nav-item").on("click", function () {
+    $(".nav-item").css("background-color", "rgb(236, 236, 236)"); // gray-300
+    $(".nav-item > p").css("color", "black");
+
+    $(this).css("background-color", "rgb(107, 114, 128)"); // gray-600
+    $(this).children("p").css("color", "white");
   });
 });
 
 //수정모드 진입
 function getEditMode() {
-  console.log("수정모드 진입");
+  isEditMode = true;
+  console.log("수정모드 진입", isEditMode);
 
   $(".delete-btn").show();
   $(".checklist-input").show();
@@ -43,7 +81,9 @@ function getEditMode() {
 
 //일반모드 진입
 function getNormalMode() {
-  console.log("일반모드 진입");
+  showPopup();
+  isEditMode = false;
+  console.log("일반모드 진입", isEditMode);
 
   $(".delete-btn").hide();
   $(".checklist-input").hide();
@@ -57,12 +97,49 @@ $(document).on("click", ".delete-btn", function () {
   $(this).closest("ul").remove();
 });
 
+//제목 nav 렌더링
+function renderNavItem(title) {
+  const navItem = $("<div>")
+    .addClass(
+      "nav-item bg-gray-600 w-12 h-12 rounded-full my-2 mr-2 flex justify-center items-center"
+    )
+    .append($("<p>").addClass("text-white text-2xl text-bold").text(title));
+
+  return navItem;
+}
+
+function initNavItem() {
+  $(".nav-item:first").css("background-color", "rgb(107, 114, 128)");
+  $(".nav-item:first > p").css("color", "white");
+
+  $(".nav-item:not(:first)").css("background-color", "rgb(236, 236, 236)");
+  $(".nav-item:not(:first) > p").css("color", "black");
+}
+
 // 체크박스를 렌더링하는 함수
 function renderCheckbox() {
   const checkboxWrapper = $("<div>").addClass("flex h-6 items-center");
+
+  const checkboxImg = $("<img>")
+    .attr({
+      src: "../../img/check_icon.svg",
+      width: "18",
+      height: "18",
+    })
+    .css({ display: "none" });
+
   const checkbox = $("<button>")
     .addClass("w-5 h-5 rounded-full border border-gray-600")
-    .css({ width: "18px", height: "18px" });
+    .append(checkboxImg)
+    .css({ width: "18px", height: "18px" })
+    .on("click", function () {
+      if (isEditMode && checkboxImg.css("display") === "none") {
+        checkboxImg.css({ display: "block" }); // 이미지를 표시
+      } else if (isEditMode && checkboxImg.css("display") === "block") {
+        checkboxImg.css({ display: "none" }); // 이미지를 숨김
+      }
+    });
+
   checkboxWrapper.append(checkbox);
   return checkboxWrapper;
 }
@@ -71,35 +148,21 @@ function renderCheckbox() {
 function renderDeleteButton() {
   const deleteBtn = $("<button>")
     .addClass("delete-btn ml-auto")
-    .css({ width: "18px", height: "18px" })
     .append(
-      $("<svg>")
-        .attr({
-          xmlns: "http://www.w3.org/2000/svg",
-          width: "18",
-          height: "18",
-          viewBox: "0 0 18 18",
-          fill: "black",
-        })
-        .append(
-          $("<path>").attr({
-            d: "M4.5 4.5L13.5 13.5M4.5 13.5L13.5 4.5L4.5 13.5Z",
-            stroke: "#3F3F46",
-            "stroke-width": "2",
-            "stroke-linecap": "round",
-            "stroke-linejoin": "round",
-          })
-        )
+      $("<img>").attr({
+        src: "../../img/delete_icon.svg",
+        width: "18",
+        height: "18",
+      })
     );
 
   return deleteBtn;
 }
 
-//태스트만 추가하고 엑스버튼이랑 동그라미는 -> 나중에 추가
+//태스크 추가
 function addNewTask(text) {
-  console.log(text);
   const newTask = $("<div>").addClass(
-    "checklist-task px-4 py-2 flex items-center"
+    "new checklist-task px-4 py-2 flex items-center"
   );
   const taskText = $("<p>").addClass("px-4 text-sm").text(text);
 
@@ -109,4 +172,21 @@ function addNewTask(text) {
   newTask.append(checkbox, taskText, deleteBtn);
 
   $(".checklist-content").append($("<ul>").append(newTask));
+}
+
+//수정사항 반영 팝업창 1초정도 나타났다가 사라짐
+function showPopup() {
+  setTimeout(() => {
+    $("#update-popup").fadeIn(500);
+    $("#update-popup").css({
+      position: "absolute",
+      top: "50%",
+      left: "40%",
+      transform: "translate(-50%, -50%)",
+    });
+
+    setTimeout(() => {
+      $("#update-popup").fadeOut(500);
+    }, 1000);
+  });
 }
