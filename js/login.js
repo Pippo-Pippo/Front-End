06f5f97dc00d58d6bc9473b24de1fe7117cf92b3
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-12 01:48:36 +0900

    Feat : 로그인 api 주소 수정

--- js/login.js
+++ js/login.js
@@ -1,35 +1,37 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
   $("button[type='submit']").on("click", function (e) {
-    e.preventDefault(); 
+    e.preventDefault();
 
-    var email = $('#email').val();
-    var password = $('#password').val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    const data = {
+      email: email,
+      password: password,
+    };
 
     $.ajax({
       type: "POST",
-      url: "https://page.ppiyong.shop/user/login",
-      data: { "email": $("input[name='email']").val() ,"password": $("input[name='password']").val()},
-      contentType: "application/json", 
-      dataType: "json", 
+      url: "https://ppiyong.shop/api/user/login",
+      data: data,
+      contentType: "application/json",
+      dataType: "json",
       success: function (response) {
-        
+        console.log(data);
         if (response.Id) {
-          document.cookie = "Id=" + response.Id; 
-          alert('로그인 성공');
+          document.cookie = "Id=" + response.Id;
+          alert("로그인 성공");
           window.location.href = "/main.html";
         } else {
-          alert('로그인 실패: 잘못된 이메일 또는 비밀번호입니다.');
+          alert("로그인 실패: 잘못된 이메일 또는 비밀번호입니다.");
         }
       },
       error: function (req, status, err) {
-    
+        console.log(data);
+
         console.log(req);
         alert("서버 요청에 실패.");
-       
-      }
+      },
     });
   });
 });
-
-
