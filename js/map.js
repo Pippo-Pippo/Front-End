c243445a01151fa559fbd46ff2267ea0b180eafc
leeeemj <leemj2944@gmail.com>
2023-08-13 04:35:49 +0900

    feat:map 최종

--- js/map.js
+++ js/map.js
@@ -9,20 +9,27 @@ clipboard.on('success', function (e) {
 clipboard.on('error', function (e) {
     console.log(e);
 });
+
+
 function out() {
     document.getElementById("modal").style.display = "none";
 }
-// var civilList = [],
-//     earthquakeList = [],
-//     allList = [];
-var marker;
 
+/********************마커 생성하기 + 커스텀 오버레이 생성******************/
+//마커 기본 설정 
+var imageRed = '/img/svg/red_marker.svg', // 마커이미지의 주소입니다
+    imageYellow = '/img/svg/yellow_marker.svg',
+    imageSize = new kakao.maps.Size(40, 50), // 마커이미지의 크기입니다
+    imageOption = { offset: new kakao.maps.Point(27, 69) },
+    image = '/img/svg/location.svg';
 
+var marker_yellow = new kakao.maps.MarkerImage(imageYellow, imageSize, imageOption);
+var marker_red = new kakao.maps.MarkerImage(imageRed, imageSize, imageOption);
 /************지도 생성************/
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(37.56645, 126.97796), // 지도의 중심좌표 내 현위치로 바꾸기
-        level: 6 // 지도의 확대 레벨
+        level: 7 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
@@ -71,45 +78,7 @@ function displayMarker(locPosition) {
 /************현재 위치 주소 받아오기************/
 
 // // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
-// kakao.maps.event.addListener(map, 'idle', function() {
-//     searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-// });
 
-// function searchAddrFromCoords(coords, callback) {
-//     // 좌표로 행정동 주소 정보를 요청합니다
-//     geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);         
-// }
-
-// function searchDetailAddrFromCoords(coords, callback) {
-//     // 좌표로 법정동 상세 주소 정보를 요청합니다
-//     geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-// }
-
-// // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
-// function displayCenterInfo(result, status) {
-//     if (status === kakao.maps.services.Status.OK) {
-//         var infoDiv = document.getElementById('centerAddr');
-
-//         for(var i = 0; i < result.length; i++) {
-//             // 행정동의 region_type 값은 'H' 이므로
-//             if (result[i].region_type === 'H') {
-//                 infoDiv.innerHTML = result[i].address_name;
-//                 break;
-//             }
-//         }
-//     }    
-// }
-
-
-
-
-
-
-//마커 생성
-var imageRed = '/img/svg/red_marker.svg', // 마커이미지의 주소입니다
-    imageYellow = '/img/svg/yellow_marker.svg',
-    imageSize = new kakao.maps.Size(40, 50), // 마커이미지의 크기입니다
-    imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
 
 /********************대피소 데이터 불러오기******************/
@@ -118,222 +87,296 @@ const categorizedData = {
     "CIVIL": []
 };
 
-// $.getJSON("../json/map.json",function(data){
-//     data.forEach(item=>{
+const EARTHQUAKE = [];
+const CIVIL = [];
+
+// $.getJSON("../json/map.json", function (data) {
+//     data.forEach(item => {
 //         categorizedData[item.category].push(item);
 //     });
 //     console.log(categorizedData);
 // });
 
+var bounds = map.getBounds();
+bounds.toString();
+var latitude_start = bounds.oa,
+    longitude_start = bounds.qa,
+    latitude_end = bounds.ha,
+    longitude_end = bounds.pa;
 
-// $.ajax({
-//         type: "GET",
-//         url: `http://page.ppiyong.shop/api/shelter`,
-//         contentType: "application/json",
-//         success: function (data) {
-//             data.forEach(item=>{
-//                 categorizedData[item.category].push(item);
-//             });
-//             console.log(categorizedData)
-//         },
-//         error: function (request, status, error) {
-//             console.log("통신실패");
-//         }
-// });
-
-// // 목데이터 불러오기
-// $.getJSON("../json/map.json", function (data) {
-//     $.each(data, function (index, item) {
-//         var name = item.name,
-//             category = item.category,
-//             lat = item.latitude,
-//             long = item.longitude,
-//             address = item.address;
-//         var value = "구분";
-//         if (category == "EARTHQUAKE") {
-//             // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-//             var markerImage = new kakao.maps.MarkerImage(imageYellow, imageSize, imageOption);
-//             value = "지진/해일 대피시설";
-//         }
-//         else {
-//             var markerImage = new kakao.maps.MarkerImage(imageRed, imageSize, imageOption);
-//             value = "민방위 대피시설";
-
-//         }
-//         var markerPosition = new kakao.maps.LatLng(long, lat); // 마커가 표시될 위치입니다 //경도, 위도
-
-//         // 마커를 생성합니다
-//         marker = new kakao.maps.Marker({
-//             position: markerPosition,
-//             image: markerImage // 마커이미지 설정 
-//         });
-
-//         // 마커가 지도 위에 표시되도록 설정합니다
-//         marker.setMap(map);
-//         var content = '<div class ="label"><span class="left"></span><span class="center">' + name + '</span><span class="right"></span></div>'
-
-//         // 커스텀 오버레이가 표시될 위치입니다 
-//         var position = new kakao.maps.LatLng(long, lat);
-
-//         // 커스텀 오버레이를 생성합니다
-//         var customOverlay = new kakao.maps.CustomOverlay({
-//             position: position,
-//             content: content,
-//             yAnchor: 3.5
-//         });
-
-//         // 커스텀 오버레이를 지도에 표시합니다
-//         customOverlay.setMap(map);
-
-//         kakao.maps.event.addListener(marker, 'click', function () {
-//             const mainDiv = $(".content_map");
-//             // Create the first inner div
-//             const div1 = document.createElement('div');
-//             div1.textContent = name;
-//             div1.className = 'ml-5 text-2xl font-bold mb-2';
-//             mainDiv.append(div1);
-
-//             // Create the second inner div with button
-//             const div2 = document.createElement('div');
-//             const button = document.createElement('button');
-//             if (category == "EARTHQUAKE") {
-//                 button.className = 'h-8 bg-yellow-600 item-center rounded-full text-xs ml-5 px-4 p-2 mb-2 text-white font-bold';
-//             }
-//             else {
-//                 button.className = 'h-8 bg-red-400 item-center rounded-full text-xs ml-5 px-4 p-2 mb-2 text-white font-bold';
-//             }
-//             button.textContent = value;
-//             div2.appendChild(button);
-
-//             mainDiv.append(div2);
-
-//             // Create the third inner div with span and button
-//             const div3 = document.createElement('div');
-//             const span = document.createElement('span');
-//             span.textContent = address;
-//             span.className = 'textArea';
-//             const copyButton = document.createElement('button');
-//             copyButton.textContent = '복사';
-//             copyButton.className = 'ml-2 p-1 rounded bg-gray-600 text-white text-sm pl-2 pr-2 pt-1 pb-1';
-//             copyButton.setAttribute('id', 'btn');
-//             copyButton.setAttribute('data-clipboard-target', '.textArea');
-//             div3.append(span);
-//             div3.append(copyButton);
-
-//             mainDiv.append(div3);
-
-//             document.getElementById("modal").style.display = "flex";
-
-
-
-//         });
-
-//     });
-// });
-
-/************************ 백엔드 통신 ***************************/
 $.ajax({
-    type: "GET",
-    url: `http://page.ppiyong.shop/api/shelter`,
-    contentType: "application/json",
-    success: function (data) {
-        console.log("통신실패");
-        $.each(data, function (index, item) {
-            var name = item.name,
-                category = item.category,
-                lat = item.latitude,
-                long = item.longitude,
-                address = item.address;
-            var value = "구분";
-            if (category == "EARTHQUAKE") {
-                // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-                var markerImage = new kakao.maps.MarkerImage(imageRed, imageSize, imageOption);
-                value = "지진/해일 대피시설";
-            }
-            else {
-                imageSize = new kakao.maps.Size(36, 40);
-                var markerImage = new kakao.maps.MarkerImage(imageYellow, imageSize, imageOption);
-                value = "민방위 대피시설";
-
-            }
-            var markerPosition = new kakao.maps.LatLng(long, lat); // 마커가 표시될 위치입니다 //경도, 위도
-
-            // 마커를 생성합니다
-            marker = new kakao.maps.Marker({
-                position: markerPosition,
-                image: markerImage // 마커이미지 설정 
+        type: "GET",
+        url: `http://page.ppiyong.shop/api/shelter${latitude_start}&${longitude_start}&${latitude_end}&${longitude_end}`,
+        contentType: "application/json",
+        success: function (data) {
+            data.forEach(item=>{
+                categorizedData[item.category].push(item);
             });
+            console.log(categorizedData)
+        },
+        error: function (request, status, error) {
+            console.log("통신실패");
+        }
+});
 
-            // 마커가 지도 위에 표시되도록 설정합니다
-            marker.setMap(map);
-            var content = '<div class ="label"><span class="left"></span><span class="center">' + name + '</span><span class="right"></span></div>'
+//받아온 거 쓸 수 있게 정리
+$(document).ready(function () {
+    for (var i = 0; i < categorizedData.EARTHQUAKE.length; i++) {
+        EARTHQUAKE.push({
+            content: '<div class ="label"><span class="left"></span><span class="center">' + categorizedData.EARTHQUAKE[i].name + '</span><span class="right"></span></div>',
+            latlng: new kakao.maps.LatLng(categorizedData.EARTHQUAKE[i].longitude, categorizedData.EARTHQUAKE[i].latitude),
+            value: "지진/해일 대피시설",
+            address: categorizedData.EARTHQUAKE[i].address,
+            name: categorizedData.EARTHQUAKE[i].name,
+            category: categorizedData.EARTHQUAKE[i].category
+        })
 
-            // 커스텀 오버레이가 표시될 위치입니다 
-            var position = new kakao.maps.LatLng(long, lat);
+    };
+    for (var i = 0; i < categorizedData.CIVIL.length; i++) {
 
-            // 커스텀 오버레이를 생성합니다
-            var customOverlay = new kakao.maps.CustomOverlay({
-                position: position,
-                content: content,
-                yAnchor: 3.5
-            });
+        CIVIL.push({
+            content: '<div class ="label"><span class="left"></span><span class="center">' + categorizedData.CIVIL[i].name + '</span><span class="right"></span></div>',
+            latlng: new kakao.maps.LatLng(categorizedData.CIVIL[i].longitude, categorizedData.CIVIL[i].latitude),
+            value: "민방위 대피시설",
+            address: categorizedData.CIVIL[i].address,
+            name: categorizedData.CIVIL[i].name,
+            category: categorizedData.CIVIL[i].category
+        })
+    };
+});
+//maker list
+var civil_custom = [],
+    earthquake_custom = [],
+    all_custom = [];
 
-            // 커스텀 오버레이를 지도에 표시합니다
-            customOverlay.setMap(map);
 
-            kakao.maps.event.addListener(marker, 'click', function () {
-                const mainDiv = $(".content");
-                // Create the first inner div
-                const div1 = document.createElement('div');
-                div1.textContent = name;
-                mainDiv.append(div1);
+var civil_marker = [],
+    earthquake_marker = [],
+    all_marker = [];
 
-                // Create the second inner div with button
-                const div2 = document.createElement('div');
-                const button = document.createElement('button');
-                if (category == "EARTHQUAKE") {
-                    button.className = 'h-8 bg-yellow-400 item-center rounded-full text-xs ml-5 px-4 p-2 text-white font-bold';
-                }
-                else {
-                    button.className = 'h-8 bg-red-400 item-center rounded-full text-xs ml-5 px-4 p-2 text-white font-bold';
-                }
-                button.textContent = value;
-                div2.appendChild(button);
 
-                mainDiv.append(div2);
+//지진 마커 생성
+$(document).ready(function () {
+    for (var i = 0; i < EARTHQUAKE.length; i++) {
+        //마커 생성
 
-                // Create the third inner div with span and button
-                const div3 = document.createElement('div');
-                const span = document.createElement('span');
-                span.textContent = address;
-                const copyButton = document.createElement('button');
-                copyButton.textContent = '복사';
-                copyButton.setAttribute('onclick', 'copy()');
-                div3.append(span);
-                div3.append(copyButton);
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: EARTHQUAKE[i].latlng,
+            image: marker_yellow,// 마커이미지 설정
+            clickable: true
+        });
 
-                mainDiv.append(div3);
+        marker.name = EARTHQUAKE[i].name;
+        marker.address = EARTHQUAKE[i].address;
+        marker.category = EARTHQUAKE[i].value;
 
-                document.getElementById("modal").style.display = "flex";
+        console.log(marker.name);
 
-            });
+        earthquake_marker.push(marker);
+        all_marker.push(marker);
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+
+        // 커스텀 오버레이를 생성합니다
+        var customOverlay = new kakao.maps.CustomOverlay({
+            position: EARTHQUAKE[i].latlng,
+            content: EARTHQUAKE[i].content,
+            yAnchor: 3.5
+        });
+
+        // 커스텀 오버레이를 지도에 표시합니다
+        customOverlay.setMap(map);
+        earthquake_custom.push(customOverlay);
+        all_custom.push(customOverlay);
+
+
+        kakao.maps.event.addListener(marker, 'click', function () {
+            openModal(marker);
+
+        });
+    }
+
+});
+
+
+//민방위 마커 
+$(document).ready(function () {
+    for (var i = 0; i < CIVIL.length; i++) {
+        //마커 생성
+
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: CIVIL[i].latlng,
+            image: marker_red,
+            clickable: true
+
+        })
+        marker.name = CIVIL[i].name;
+        marker.address = CIVIL[i].address;
+        marker.category = CIVIL[i].value;
+        console.log(marker.name); //두개 찍힘
+        //마커 클릭하면 두번째마커의 정보만 뜸. 왜지 ..?
+
+        civil_marker.push(marker);
+        all_marker.push(marker);
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+
+        // 커스텀 오버레이를 생성합니다
+        var customOverlay = new kakao.maps.CustomOverlay({
+            position: CIVIL[i].latlng,
+            content: CIVIL[i].content,
+            yAnchor: 3.5
+        });
+
+        // 커스텀 오버레이를 지도에 표시합니다
+        customOverlay.setMap(map);
+
+        civil_custom.push(customOverlay);
+        all_custom.push(customOverlay);
+
+
+        kakao.maps.event.addListener(marker, 'click', function () {
+            openModal(marker);
 
         });
 
-    },
-    error: function (request, status, error) {
-        alert(
-        "code:" +
-            request.status +
-            "\n" +
-            "message:" +
-            request.responseText +
-            "\n" +
-            "error:" +
-            error
-        );
-    },
+
+    }
+
 });
+
+/********************카테고리따라 마커 보이기 숨기기******************/
+
+//마커 표시 or 삭제하는 함수
+function setMarkers(map, list) {
+    for (var i = 0; i < list.length; i++) {
+        list[i].setMap(map);
+    }
+}
+//마커 보이기 함수
+function showMarkers(list) {
+    setMarkers(map, list)
+}
+//마커 감추기 함수
+function hideMarkers(list) {
+    setMarkers(null, list);
+}
+function earthquake() {
+    hideMarkers(civil_marker);
+    showMarkers(earthquake_marker);
+
+    hideMarkers(civil_custom);
+    showMarkers(earthquake_custom);
+}
+function civil() {
+    hideMarkers(earthquake_marker);
+    showMarkers(civil_marker);
+
+    hideMarkers(earthquake_custom);
+    showMarkers(civil_custom);
+
+}
+function go() {
+    showMarkers(all_marker);
+    showMarkers(all_custom);
+}
+
+/********************마커 정보창 뜨게 하기******************/
+function openModal(marker) {
+    // the first inner div
+    console.log(marker.name);
+    console.log(marker.address);
+    console.log(marker.category);
+
+    const div1 = document.getElementById('name');
+    div1.innerText = marker.name;
+    const div2 = document.getElementById('textArea');
+    div2.innerText = marker.address;
+    const div3 = document.getElementById('category_btn');
+    div3.innerHTML = marker.category;
+
+
+    const btn = $('#category_btn');
+    if (marker.category == "민방위 대피시설") {
+        btn.attr('class', 'h-8 bg-red-400 item-center rounded-full text-xs ml-5 px-4 p-2 mb-2 text-white font-bold mt-1');
+    }
+    else {
+        btn.attr('class', 'h-8 bg-yellow-600 item-center rounded-full text-xs ml-5 px-4 p-2 mb-2 text-white font-bold mt-1');
+    }
+
+
+
+    document.getElementById("modal").style.display = "flex";
+
+
+}
+
+//현재 좌표로 주소변환해서 추가하기
+function reverseGeocode(lat, lng, callback) {
+    const geocoder = new google.maps.Geocoder();
+    const latLng = { lat: lat, lng: lng };
+
+    geocoder.geocode({ location: latLng }, function (results, status) {
+        if (status === "OK") {
+            if (results[0]) {
+                const addressComponents = results[0].address_components;
+                console.log(addressComponents);
+
+                let city = null; // 시/도/광역시 정보를 저장할 변수
+                let district = null; // 구/군 정보를 저장할 변수
+                let sub_district = null; // 구/군 정보를 저장할 변수
+
+                for (let component of addressComponents) {
+                    if (component.types.includes("administrative_area_level_1")) {
+                        city = component.long_name;
+                        console.log(city);
+                    }
+                    if (
+                        component.types.includes("sublocality_level_1") ||
+                        component.types.includes("locality")
+                    ) {
+                        district = component.long_name;
+                        console.log(district);
+                    }
+                    if (component.types.includes("sublocality_level_2")) {
+                        sub_district = component.long_name;
+                        console.log(sub_district);
+                    }
+                }
+
+                if (city && district && sub_district) {
+                    callback(`${city} ${district} ${sub_district}`);
+                } else if (city && district) {
+                    callback(`${city} ${district} `);
+                } else if (city) {
+                    callback(city);
+                } else {
+                    callback("시/도/광역시 또는 구/군 정보를 찾을 수 없습니다.");
+                }
+            } else {
+                callback("결과가 없습니다.");
+            }
+        } else {
+            callback("Geocoder failed due to: " + status);
+        }
+    });
+}
+
+//현재 접속 좌표 받아와서 주소로 변환하기
+navigator.geolocation.getCurrentPosition(function (position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    reverseGeocode(lat, lon, function (address) {
+        console.log(lat, lon, address);
+        $("#current_city").text(address);
+    });
+});
+
+
+
 
 
 
