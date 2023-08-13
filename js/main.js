5c024f1d1636f69ac648410ee30a10dd8d3941f0
yun0329 <yunjin0329@naver.com>
2023-08-13 23:51:40 +0900

    fix :메인 수정(강윤진)

--- js/main.js
+++ js/main.js
@@ -1,56 +1,58 @@
 $(document).ready(function () {
-  var height = $(".newsList").height();
-  var move = 0;
-  var noticeRollingOff;
-
-  function noticeRolling() {
-    move += height;
-    $(".news-list").animate({ top: -move }, 600, function () {
-      if (move >= $(".news-list li").length * height) {
-        $(this).css("top", 0);
-        move = 0;
-      }
+  function getNewsList() {
+    return $.ajax({
+      type: "GET",
+      url: "https://page.ppiyong.shop/api/home",
+      dataType: "json",
     });
   }
 
-  noticeRollingOff = setInterval(noticeRolling, 3000);
-
-  $(".news-list_stop").click(function () {
-    clearInterval(noticeRollingOff);
-  });
-
-  $(".news-list_start").click(function () {
-    noticeRollingOff = setInterval(noticeRolling, 1000);
-  });
-
-  //json (뉴스)
-  function loadnews() {
-    return fetch("json/main.json")
-      .then((response) => response.json())
-      .then((json) => json.news)
-      .catch((error) => {
-        console.log("실패");
-        console.error(error);
-      });
-  }
-
-  loadnews().then((news) => {
-    displayNews(news);
-  });
-
   function displayNews(news) {
-    console.log(news);
     const container = document.getElementById("news-item");
     container.innerHTML = news.map((item) => createHTMLString(item)).join("");
   }
 
   function createHTMLString(news) {
-    console.log("createhtml");
     return `
       <li class="news-item">
-        <a href="#" class="link">${news.content}</a>
+        <a href="${news.url}" class="link">${news.title}</a>
       </li>`;
   }
+
+  getNewsList().done(function (data) {
+    const newsList = data.news;
+    displayNews(newsList);
+
+    // News rolling functionality
+    var height = $(".newsList").height();
+    var move = 0;
+    var noticeRollingOff;
+
+    function noticeRolling() {
+      move += height;
+      $(".news-list").animate({ top: -move }, 600, function () {
+        if (move >= $(".news-list li").length * height) {
+          $(this).css("top", 0);
+          move = 0;
+        }
+      });
+    }
+
+    noticeRollingOff = setInterval(noticeRolling, 3000);
+
+    $(".news-list_stop").click(function () {
+      clearInterval(noticeRollingOff);
+    });
+
+    $(".news-list_start").click(function () {
+      noticeRollingOff = setInterval(noticeRolling, 1000);
+    });
+    alert("통신 성공");
+
+  }).fail(function (error) {
+    console.error("Failed to retrieve news data:", error);
+    alert("통신 실패");
+  });
 });
 
 // json (날씨)
