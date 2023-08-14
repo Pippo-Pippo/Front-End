8bfca4527a41fcf497a7c91f78dc595e3dd80532
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-14 20:45:44 +0900

    아 커밋 잘못했다

--- js/myChecklist.js
+++ js/myChecklist.js
@@ -435,3 +435,20 @@ $(document).on("keypress", ".editInput", function (event) {
     $(this).blur(); // blur 이벤트 핸들러를 호출하여 위의 로직을 실행
   }
 });
+
+//공유하기 누르면 이미지(png)로 다운로드
+function downImg() {
+  alert("체크리스트 이미지가 저장되었어요.");
+  html2canvas($(".my-checklist")[0]).then(function (canvas) {
+    var myImage = canvas.toDataURL();
+    downloadURI(myImage, "myChecklist.png");
+  });
+}
+
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+}
