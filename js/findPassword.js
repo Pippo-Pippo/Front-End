6e825e4686e342ae0cb7e546daca75b50069367b
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-15 17:24:10 +0900

    ajax post 수정

--- js/findPassword.js
+++ js/findPassword.js
@@ -1,28 +1,27 @@
-$(document).ready(function() {
-    $("button[type='submit']").on("click", function(e) {
-      e.preventDefault();
-  
-      var email = $('#email').val();
-  
-      $.ajax({
-        type: "POST",
-        url: "https://ppiyong.shop/api/user/findPw",
-        data: JSON.stringify({
-          'email': email,
-        }),
-        contentType: "application/json",
-        dataType: "json",
-        success: function(response) {
-          if (response.success) {
-            alert('임시 비밀번호가 이메일로 전송되었습니다.');
-          } else {
-            alert('비밀번호 찾기 실패: 해당 이메일 주소가 존재하지 않습니다.');
-          }
-        },
-        error: function(xhr, status, error) {
-          console.error(xhr, status, error);
-          alert('서버 요청 실패: 서버에 문제가 발생하였습니다.');
+$(document).ready(function () {
+  $("button[type='submit']").on("click", function (e) {
+    e.preventDefault();
+
+    var email = $("#email").val();
+    console.log(email);
+
+    $.ajax({
+      type: "POST",
+      url: `https://ppiyong.shop/api/user/findPw?email=${email}`,
+
+      contentType: "application/json",
+      dataType: "json",
+      success: function (response) {
+        if (response.success) {
+          alert("임시 비밀번호가 이메일로 전송되었습니다.");
+        } else {
+          alert("비밀번호 찾기 실패: 해당 이메일 주소가 존재하지 않습니다.");
         }
-      });
+      },
+      error: function (xhr, status, error) {
+        console.error(xhr, status, error);
+        alert("서버 요청 실패: 서버에 문제가 발생하였습니다.");
+      },
     });
   });
+});
