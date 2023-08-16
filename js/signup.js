859bdf196d33072e62d1ef04a1b83c84e1c687f9
yun0329 <yunjin0329@naver.com>
2023-08-17 03:59:10 +0900

    회원가입

--- js/signup.js
+++ js/signup.js
@@ -7,7 +7,7 @@ function signup() {
 
   $.ajax({
     type: "POST",
-    url: "https://ppiyoung.shop/api/user/register",
+    url: "https://ppiyong.shop/api/user/register",
     contentType: "application/json",
     data: JSON.stringify({
       email: email,
@@ -94,6 +94,8 @@ function goNext() {
   const signupWarp = document.getElementById("signup-wrap");
   const signupTitle = document.getElementById("signup-title");
   const nextButton = document.getElementById("nextButton");
+  const heading = document.getElementById("heading");
+  
   // "회원가입" 내용이 사라진 후에 실행
   signupWarp.style.display = "none"; //없어지기
   signupTitle.innerHTML = "회원정보 입력"; //innerText 수정
