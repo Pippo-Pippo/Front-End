c36130fee639eef3fc291fc5ae73e20f419ce0b9
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-16 17:09:10 +0900

    Feat: 로그인로직 적용네비바 추가

--- js/isLogined.js
+++ js/isLogined.js
@@ -1,19 +1,44 @@
 $(document).ready(function () {
-  function checkJSessionID() {
-    var cookies = document.cookie.split(";");
-    var jsessionExists = false;
-
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      if (cookie.indexOf("JSESSIONID=") === 0) {
-        jsessionExists = true;
-        break;
-      }
-    }
-
-    return jsessionExists;
-  }
-
   var isLoggedin = checkJSessionID();
   console.log(isLoggedin); // true 또는 false
+
+  if (isLoggedin) {
+    getUserInform();
+    $("#login-nav").show();
+    $("#non-login-nav").hide();
+  } else {
+    $("#non-login-nav").show();
+    $("#login-nav").hide();
+  }
+
+  function getUserInform() {
+    $.ajax({
+      url: "https://ppiyong.shop/api/user",
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        username = data.nickName;
+        $(".username-text").text(username);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        alert("통신 실패");
+        console.error(textStatus, errorThrown);
+      },
+    });
+  }
 });
+
+function checkJSessionID() {
+  var cookies = document.cookie.split(";");
+  var jsessionExists = false;
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.indexOf("JSESSIONID=") === 0) {
+      jsessionExists = true;
+      break;
+    }
+  }
+
+  return jsessionExists;
+}
