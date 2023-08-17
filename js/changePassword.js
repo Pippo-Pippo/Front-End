cd993ba3ee6624ac8563de634688da028b51c5dc
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-18 01:43:02 +0900

    credentials 추가

--- js/changePassword.js
+++ js/changePassword.js
@@ -1,34 +1,36 @@
-$(document).ready(function() {
-  $("button[type='submit']").on("click", function(e) {
-      e.preventDefault();
+$(document).ready(function () {
+  $("button[type='submit']").on("click", function (e) {
+    e.preventDefault();
 
-      var password = $('#password').val();
-      var passwordConfirmation = $('#passwordConfirmation').val();
+    var password = $("#password").val();
+    var passwordConfirmation = $("#passwordConfirmation").val();
 
-      if (password !== passwordConfirmation) {
-        alert('새 비밀번호와 재입력한 비밀번호가 일치하지 않습니다.');
-        return;
+    if (password !== passwordConfirmation) {
+      alert("새 비밀번호와 재입력한 비밀번호가 일치하지 않습니다.");
+      return;
     }
-    
 
     $.ajax({
-        type: 'PUT',
-        url: "https://ppiyong.shop/api/user/pw",
-        contentType: 'application/json',
-        data: JSON.stringify({
-            'password': password,
-        }),
-        success: function(data){
-            alert('비밀번호가 성공적으로 변경되었습니다.');
-            window.location.href = "/main.html";
-
-        },  error: function(xhr, status, error) {
-            if (xhr.status === 403) {
-              alert('권한 없는 유저입니다.'); 
-            } else {
-              alert('서버 요청 실패: 서버에 문제가 발생하였습니다.');
-            }
-          }
-    })
+      type: "PUT",
+      url: "https://ppiyong.shop/api/user/pw",
+      contentType: "application/json",
+      data: JSON.stringify({
+        password: password,
+      }),
+      xhrFields: {
+        withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
+      },
+      success: function (data) {
+        alert("비밀번호가 성공적으로 변경되었습니다.");
+        window.location.href = "/main.html";
+      },
+      error: function (xhr, status, error) {
+        if (xhr.status === 403) {
+          alert("권한 없는 유저입니다.");
+        } else {
+          alert("서버 요청 실패: 서버에 문제가 발생하였습니다.");
+        }
+      },
+    });
   });
 });
--- js/findPassword.js
+++ js/findPassword.js
@@ -8,17 +8,19 @@ $(document).ready(function () {
     var password = "임시비밀번호";
 
     $.ajax({
-      type: 'POST',
+      type: "POST",
       url: `https://ppiyong.shop/api/user/findPw?email=${email}`,
-      
-      contentType: 'application/json',
-    
-      success: function(data){
+      xhrFields: {
+        withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
+      },
+      contentType: "application/json",
+
+      success: function (data) {
         alert("임시 비밀번호가 이메일로 전송되었습니다.");
-      }, 
-      error: function(request, status, error){
+      },
+      error: function (request, status, error) {
         alert("서버 요청 실패: 서버에 문제가 발생하였습니다.");
-      }
-    })
+      },
+    });
   });
 });
