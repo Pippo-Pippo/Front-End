55abfece0ff9968070db4ef6951df1eaf1880769
yun0329 <yunjin0329@naver.com>
2023-08-18 14:13:28 +0900

    링크 수정

--- js/isLoggedin.js
+++ js/isLoggedin.js
@@ -1,37 +1,38 @@
 $(document).ready(function () {
   var isLoggedin;
-  console.log(isLoggedin); // true 또는 false
+  getUserInform();
 
-  function getUserInform() {
-    $.ajax({
-      url: "https://ppiyong.shop/api/user",
-      type: "GET",
-      xhrFields: {
-        withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
-      },
-      success: function (data) {
-        isLoggedin = false;
-        $(".username-text").text(data.nickName);
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        if (jqXHR.status === 403) {
-          isLoggedin = false;
-        }
-
-        console.error(textStatus, errorThrown);
-        $(".username-text").text("undefined");
-      },
-    });
-  }
-
-  if (isLoggedin) {
-    $("#login-nav").show();
-    $("#non-login-nav").hide();
-  } else {
-    $("#non-login-nav").show();
-    $("#login-nav").hide();
-  }
+  $("#non-login-nav").show();
+  $("#login-nav").hide();
 });
+
+function getUserInform() {
+  $.ajax({
+    url: "https://ppiyong.shop/api/user",
+    type: "GET",
+    xhrFields: {
+      withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
+    },
+    success: function (data) {
+      isLoggedin = true;
+      $(".username-text").text(data.nickName);
+
+      $("#login-nav").show();
+      $("#non-login-nav").hide();
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      if (jqXHR.status === 403) {
+        isLoggedin = false;
+
+        $("#non-login-nav").show();
+        $("#login-nav").hide();
+      }
+
+      console.error(textStatus, errorThrown);
+      $(".username-text").text("undefined");
+    },
+  });
+}
 
 function checkJSessionID() {
   var cookies = document.cookie.split(";");
