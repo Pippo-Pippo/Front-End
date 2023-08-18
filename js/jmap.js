a3a299cc192f283565c90c2b1406b7a1d95da9eb
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-18 19:18:48 +0900

    제발요

--- js/jmap.js
+++ js/jmap.js
@@ -41,14 +41,14 @@ $(document).ready(function () {
     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
     navigator.geolocation.getCurrentPosition(function (position) {
       var lat = position.coords.latitude; // 위도
-      var on = position.coords.longitude; // 경도
+      var lon = position.coords.longitude; // 경도
       console.log(lat);
       console.log(lon);
       locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
 
       // 마커와 인포윈도우를 표시합니다
       displayMarker(locPosition);
-      console.log(locPosition);
+      cosnsole.log(locPosition);
       getShelterData();
     });
   } else {
