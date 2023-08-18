871a83ac06d2e191856813ca0840dcaf7ab002cb
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-18 19:34:57 +0900

    제발요

--- js/jmap.js
+++ js/jmap.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
 
       // 마커와 인포윈도우를 표시합니다
       displayMarker(map, locPosition);
-      cosnsole.log(locPosition);
+      console.log(locPosition);
       getShelterData(lat, lon);
     });
   } else {
