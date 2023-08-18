700e06f1f278bdb1dbcc0326f43d98dc391b0f17
leeeemj <leemj2944@gmail.com>
2023-08-18 14:32:36 +0900

    로그아웃

--- js/logoutTest.js
+++ js/logoutTest.js
@@ -1,40 +1,74 @@
 var isLoggedin=null;
+function checkJSessionID() {
+ var cookies = document.cookie.split(";");
+ var jsessionExists = false;
+
+ for (var i = 0; i < cookies.length; i++) {
+   var cookie = cookies[i].trim();
+   if (cookie.indexOf("JSESSIONID=") === 0) {
+      jsessionExists = true;
+      break;
+  }
+}
+
+
+ return jsessionExists;
+}
+
+
+
+
 $(document).ready(function () {
+  var isLoggedin;
+  getUserInform();
 
-    isLoggedin = checkJSessionID();
-    console.log(isLoggedin); // true 또는 false
-
-    function getUserInform() {
-      $.ajax({
-      url: "https://ppiyong.shop/api/user",
-      type: "GET",
-      xhrFields: {
-        withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
-      },
-      success: function (data) {
-      username = data.nickName;
-      $(".username-text").text(username);
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        alert("통신 실패");
-        console.error(textStatus, errorThrown);
-        $(".username-text").text("undefined");
-      },
-    });
-  }
-
- 
-
-  if (isLoggedin) {
-    getUserInform();
-    $("#login-nav").show();
-    $("#non-login-nav").hide();
-    } else {
-    $("#non-login-nav").show();
-    $("#login-nav").hide();
-  }
-
+  $("#non-login-nav").show();
+  $("#login-nav").hide();
 });
+
+function getUserInform() {
+  $.ajax({
+    url: "https://ppiyong.shop/api/user",
+    type: "GET",
+    xhrFields: {
+      withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
+    },
+    success: function (data) {
+      isLoggedin = true;
+      $(".username-text").text(data.nickName);
+
+      $("#login-nav").show();
+      $("#non-login-nav").hide();
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      if (jqXHR.status === 403) {
+        isLoggedin = false;
+
+        $("#non-login-nav").show();
+        $("#login-nav").hide();
+      }
+
+      console.error(textStatus, errorThrown);
+      $(".username-text").text("undefined");
+    },
+  });
+}
+
+function checkJSessionID() {
+  var cookies = document.cookie.split(";");
+  var jsessionExists = false;
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.indexOf("JSESSIONID=") === 0) {
+      jsessionExists = true;
+      break;
+    }
+  }
+
+  return jsessionExists;
+}
+
 $('#logout_btn').click(function () {
   console.log("로그아웃 버튼 클릭");
   logout();
@@ -62,23 +96,4 @@ function logout() {
       }); 
     }
 
-
-
-
-
-function checkJSessionID() {
- var cookies = document.cookie.split(";");
- var jsessionExists = false;
-
- for (var i = 0; i < cookies.length; i++) {
-   var cookie = cookies[i].trim();
-   if (cookie.indexOf("JSESSIONID=") === 0) {
-      jsessionExists = true;
-      break;
-  }
-}
-
-
- return jsessionExists;
-}
 
